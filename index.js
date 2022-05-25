702e58e3924eac4c5f92de09f315223c22530639
md-atiqurrahman <mdatiqurrahman9104@gmail.com>
2022-05-26 03:35:26 +0600

    connect with mongodb

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -8,10 +9,35 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) =>{
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ldsyr.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+     try{
+        await client.connect();
+
+        const serviceCollection = client.db('doctors-portal').collection('service');
+
+        app.get('/service', async (req, res)=>{
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+     }
+     finally{
+        //  await client.close();
+     }
+}
+
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
     res.send('Doctors server is running');
 });
 
-app.listen(port,() =>{
-    console.log('Doctors app listening on port',port)
+app.listen(port, () => {
+    console.log('Doctors app listening on port', port)
 })
